4e93cc79eab27e515c00db8654fbdfa8b0eee45f
shelmith <shelwangui23@gmail.com>
2025-06-09 15:00:47 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+// Functions that return first and last two drivers
+const returnFirstTwoDrivers = function(drivers) {
+  return drivers.slice(0, 2);
+};
+
+const returnLastTwoDrivers = function(drivers) {
+  return drivers.slice(-2);
+};
+
+// Array containing the above functions
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+// Function that returns a fare multiplier function
+function createFareMultiplier(multiplier) {
+  return function(fare) {
+    return fare * multiplier;
+  };
+}
+
+// fareDoubler uses createFareMultiplier
+const fareDoubler = createFareMultiplier(2);
+
+// fareTripler uses createFareMultiplier
+const fareTripler = createFareMultiplier(3);
+
+// Function that applies one of the selectingDrivers
+function selectDifferentDrivers(drivers, driverSelector) {
+  return driverSelector(drivers);
+}
